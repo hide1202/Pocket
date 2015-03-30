81ce0c3e39c5ad25c381eff9c24ecea4ad7f470c
hide1202 <hide1202@naver.com>
2015-03-30 16:56:01 +0900

    Add PocketSqlManager which help to use SQLite easily. Add PocketBase in order to use SQLite automatically.

---
+++ Pocket/Pocket.h
@@ -0,0 +1,19 @@
+//
+//  Pocket.h
+//  Pocket
+//
+//  Created by VIEWPOINT on 2015. 3. 29..
+//  Copyright (c) 2015년 VIEWPOINT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pocket.
+FOUNDATION_EXPORT double PocketVersionNumber;
+
+//! Project version string for Pocket.
+FOUNDATION_EXPORT const unsigned char PocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pocket/PublicHeader.h>
+
+
---
+++ PocketTests/PocketSqlManagerTest.m
@@ -0,0 +1,51 @@
+//
+//  PocketBaseTest.m
+//  Pocket
+//
+//  Created by VIEWPOINT on 2015. 3. 29..
+//  Copyright (c) 2015년 VIEWPOINT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "PocketSqlManager.h"
+
+@interface PocketSqlManagerTest : XCTestCase
+
+@end
+
+@implementation PocketSqlManagerTest
+{
+	PocketSqlManager* _sqlManager;
+}
+
+- (void)setUp {
+    [super setUp];
+	_sqlManager = [[PocketSqlManager alloc] initWithDbName:@"testtable.db"];
+	[_sqlManager executeQuery:@"create table test(num integer)"];
+}
+
+- (void)tearDown {
+    [super tearDown];
+	NSLog(@"Delete database result : %@", [_sqlManager deleteDatabase] ? @"true" : @"false");
+}
+
+- (void)testExample {
+	XCTestExpectation * expection = [self expectationWithDescription:@"select test"];
+	BOOL result1 = [_sqlManager executeQuery:@"insert into test(num) values(1)"];
+	
+	[_sqlManager executeQuery:@"select * from test" resultHandler:^(NSArray *result) {
+		XCTAssert([[result objectAtIndex:0] isEqual:@1], @"Pass");
+		[expection fulfill];
+	}];
+    XCTAssert(result1, @"Pass");
+	
+	[self waitForExpectationsWithTimeout:1 handler:^(NSError *error) {
+	}];
+}
+
+- (void)testPerformanceExample {
+    [self measureBlock:^{
+    }];
+}
+
+@end
